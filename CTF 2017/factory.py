9bb124ef7a09e6c0e4a672904b1181e17fbcec7b
ASHIJANKEN <24617794+ASHIJANKEN@users.noreply.github.com>
2018-05-13 15:47:48 +0900

    Update factory.py

--- CTF 2017/factory.py
+++ CTF 2017/factory.py
@@ -5,7 +5,6 @@ N, K = map(int, input().split())
 machines = []
 for i in range(N):
   machines.append(list(map(int, input().split())))
-  machines[i] = machines[i] + [0]
 
 heapq.heapify(machines)
 
@@ -13,7 +12,6 @@ total_time = 0
 for i in range(K):
   m = heapq.heappop(machines)
   total_time += m[0]
-  m[2] += 1
   m[0] += m[1]
   heapq.heappush(machines, m)
 
